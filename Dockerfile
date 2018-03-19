e143a172090c33f926021e8ab2d50300bafade64
JuanitoAli <jcflorezv@unal.edu.co>
2018-03-19 16:07:55 -0500

    :

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM ruby:2.3
+
+RUN mkdir /stats-ms
+WORKDIR /stats-ms
+
+ADD Gemfile /stats-ms/Gemfile
+ADD Gemfile.lock /stats-ms/Gemfile.lock
+
+RUN bundle install
+ADD . /stats-ms
--- app/models/disposal_point.rb
+++ app/models/disposal_point.rb
@@ -1,8 +1,22 @@
+#0 Nombre Punto de Recolección      disposal_point_name
+#1 Dirección punto de recolección,  disposal_point_address
+#2 Ciudad,                          city
+#3 Departamento,                    department
+#4 País,                            country
+#5 Categoria residuo,               residue_category
+#6 Tipo Residuo,                    residue_type
+#7 Nombre residuo,                  residue_name
+#8 Ubicacion,                       location
+#9 Horario,                         schedule
+#A Nombre Programa Posconsumo,      postconsumption_program_name
+#B Persona Contacto,                contact_person
+#C Correo electrónico               email
+
 class DisposalPoint < ApplicationRecord
     def self.count_by(column)
         DisposalPoint.group(column).count
     end
-    
+
     def self.person_disposal_ratio
       @ratios = {}
       DisposalPoint.count_by("department").each do |department, count|
